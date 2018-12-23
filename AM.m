30377494b3454e07afb034e1188d9aaa087e179a
Hady Maher <hadymaher31@yahoo.com>
2018-12-23 23:13:23 +0200

    finish the project and add docs

--- AM.m
+++ AM.m
@@ -8,24 +8,28 @@ Ac = abs(min(audio) / 0.9);
 carr_freq = 100000;
 carr_sample_rate = carr_freq* 3;
 % resample the audio to the carrier sample rate
-audio = resample(audio, carr_sample_rate, sample_rate);
+audio_resample = resample(audio, carr_sample_rate, sample_rate);
 % carrier omega
 Wc = 2 * pi * carr_freq;
 % time vector
-size_of_audio = size(audio);
+size_of_audio = size(audio_resample);
 t = (1:size_of_audio(1));
 t = transpose(t);
 % modulated signal
-St = (Ac + audio(t)) .* cos(Wc * t);
+St = (Ac + audio_resample(t)) .* cos(Wc * t);
 % loop in SNR from 0 to 20 dB
+MSE = (0:20);
 for SNR = 0:20
     % calculate the signal with noise
     audio_noise = awgn(St, SNR);
-    % calcualte the demodulated signal
+    % calculate the demodulated signal
     demod = abs(hilbert(audio_noise)) - mean(abs(hilbert(audio_noise)));
     % resample it to the base sample rate
     demod = resample(demod, sample_rate, carr_sample_rate);
+    new_MSE = mean((audio - demod).^2);
+    MSE(SNR+1) = new_MSE;
     % listen to the signal
     sound(demod);
     pause(5);
 end
+plot((0:20), MSE);
--- FM.m
+++ FM.m
@@ -1,30 +1,39 @@
-% pkg load communications
-%Original Signal
-[x, fs] = audioread('speech_dft_8khz.wav');
-sound(x, fs, 16);
-plot(x)
-
-%Modulated Signal
-%fm = 'New.wav' 
-%t = 0:1/fs:10;
-%w=2*pi*100000*t;
-%audiowrite(fm, cos(cos(w/1500).*w), fs);
-%[y,fs]=audioread('New.wav');
-%sound(y, fs, 16);
-%plot (y);
-
-% FM Modualtion
-fdev = fc/5;
-audiowrite(fm, fmmod(x,100000,fs,fdev), fs);
-[y,fs]=audioread('New.wav');
-sound(y, fs, 16);
-plot (y);
-
-%Add Random Noise
-noised = awgn(y,10,'measured');
-
-%Demodulate
-audiowrite(fm, fmdemod(noised,fc,fs,fDev), fs);
-[z,fs]=audioread('New.wav');
-sound(z, fs, 16);
-plot (z);
+% read from audio file
+[audio, sample_rate] = audioread('speech_dft_8khz.wav');
+% make sure that the audio is 1D vector
+audio = audio(:,1);
+% DC component
+Ac = 1;
+% carrier frequency and sample rate
+carr_freq = 100000;
+carr_sample_rate = carr_freq* 3;
+% resample the audio to the carrier sample rate
+audio_resample = resample(audio, carr_sample_rate, sample_rate);
+% carrier omega
+Wc = 2 * pi * carr_freq;
+% time vector
+size_of_audio = size(audio_resample);
+x = (1:size_of_audio(1));
+x = transpose(x);
+% modulated signal
+B = 20000;
+m_beta = 5;
+Kf = (m_beta * B * 2 * pi) / abs(max(audio_resample));
+int_audio = cumsum(audio_resample(x)) / carr_sample_rate;
+St = Ac * cos((Wc * x) + (Kf * int_audio));
+% loop in SNR from 0 to 20 dB
+MSE = (0:20);
+for SNR = 0:20
+    % calculate the signal with noise
+    audio_noise = awgn(St, SNR);
+    % calculate the demodulated signal
+    demod = abs(fmdemod(audio_noise, carr_freq, carr_sample_rate, m_beta * B));
+    % resample it to the base sample rate
+    demod = resample(demod, sample_rate, carr_sample_rate);
+    new_MSE = mean((audio - demod).^2);
+    MSE(SNR+1) = new_MSE;
+    % listen to the signal
+    sound(demod);
+    pause(5);
+end
+plot((0:20), MSE);
